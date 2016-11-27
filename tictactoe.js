d2718fe5996eb447558430844fe1d7345b973402
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-11-27 21:21:16 +0100

    Move reducer to a separate file

--- tictactoe.js
+++ tictactoe.js
@@ -3,57 +3,10 @@ const ReactDOM = require('react-dom');
 const { createStore, applyMiddleware } = require('redux');
 const { Provider, connect } = require('react-redux');
 const ReduxThunk = require('redux-thunk').default;
-const { isFinished, gameStatusMessage, getStateAfterMove, chooseResponseMove } = require('./game.js');
+
 const firebase = require('./firebase.js');
 const { writeUserState, login, isAuth, initAuth } = require('./firebase.js')
-
-const defaultState = { board: '         '.split(''), player: '?', status: 'wait', level: '' };
-
-const tictactoe = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'MOVE_USER':
-      if (state.player !== '?' && state.status === 'running' && state.level !== '' && state.board[action.index] === ' ') {
-        const stateAfterUserMove = Object.assign({}, state, getStateAfterMove(state.board, state.player, action.index), { status: 'delay' });
-        if (isFinished(stateAfterUserMove.board)) {
-          return Object.assign({}, stateAfterUserMove, { status: gameStatusMessage(stateAfterUserMove.board) });
-        } else {
-          return stateAfterUserMove;
-        }
-      } else {
-        return state;
-      }
-
-    case 'MOVE_SELF':
-      if (state.player !== '?' && state.status === 'delay' && state.level !== '') {
-        const nextMove = chooseResponseMove(state.board, state.player, state.level);
-        const stateAfterBothMoves = Object.assign({}, state, getStateAfterMove(state.board, state.player, nextMove), { status: 'running' });
-        if (!isFinished(stateAfterBothMoves.board)) {
-          return stateAfterBothMoves;
-        } else {
-          return Object.assign({}, stateAfterBothMoves, { status: gameStatusMessage(stateAfterBothMoves.board) })
-        }
-      } else {
-        return state;
-      }
-
-    case 'CHOOSE_SYMBOL':
-      return Object.assign({}, state, { player: action.symbol, status: 'running' });
-
-    case 'CHOOSE_LEVEL':
-      return Object.assign({}, state, { level: action.level });
-
-    case 'RESET':
-      return defaultState;
-
-    case 'RECEIVE_STATE':
-      return action.payload;
-
-    default:
-      return state;
-  }
-}
-
-module.exports = { tictactoe };
+const { tictactoe } = require('./reducer-tictactoe.js');
 
 const store = createStore(tictactoe, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(ReduxThunk));
 
